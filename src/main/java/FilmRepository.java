f1eddbb47fa143e946e06ba935f99b2b67db5f81
KolyanGrom <kolyangrom@yandex.ru>
2024-03-06 16:37:17 +0700

    Окончательный вариант решения ДЗ

--- src/main/java/FilmRepository.java
+++ src/main/java/FilmRepository.java
@@ -1,8 +1,28 @@
+class PosterItems {
+    private int id;
+    private String genreFilm;
+    private String filmName;
+
+    public PosterItems(int id, String genreFilm, String filmName) {
+        this.id = id;
+        this.genreFilm = genreFilm;
+        this.filmName = filmName;
+    }
+}
+
 public class FilmRepository {
     private PosterItems[] items = new PosterItems[0];
+    private int limit;
 
+    public FilmRepository() {
+        this.limit = 5;
+    }
 
-    public void save(PosterItems item) {
+    public FilmRepository(int limit) {
+        this.limit = limit;
+    }
+
+    public void add(PosterItems item) {
         PosterItems[] tmp = new PosterItems[items.length + 1];
         for (int i = 0; i < items.length; i++) {
             tmp[i] = items[i];
@@ -10,29 +30,25 @@ public class FilmRepository {
         }
         tmp[tmp.length - 1] = item;
         items = tmp;
-
     }
 
-    public PosterItems[] getItems() {
+    public PosterItems[] findAll() {
         return items;
     }
 
-    public void removeFilm(int id) {
-
-        PosterItems[] tmp = new PosterItems[items.length - 1];
-        int copyToIndex = 0;
-        for (PosterItems item : items) {
-            if (item.getId() != id) {
-                tmp[copyToIndex] = item;
-                copyToIndex++;
-            }
+    public PosterItems[] findLast() {
+        int resultLenght;
+        if (limit < items.length) {
+            resultLenght = limit;
+        } else {
+            resultLenght = items.length;
+            ;
         }
-        items = tmp;
 
+        PosterItems[] result = new PosterItems[resultLenght];
+        for (int i = 0; i < resultLenght; i++) {
+            result[i] = items[items.length - 1 - i];
+        }
+        return result;
     }
 }
-
-
-
-
-
--- src/test/java/FilmRepositoryTest.java
+++ src/test/java/FilmRepositoryTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FilmRepositoryTest {
-
     PosterItems item1 = new PosterItems(1, "Боевик", "Бладшот");
     PosterItems item2 = new PosterItems(2, "Мультфильм", "Вперёд");
     PosterItems item3 = new PosterItems(3, "Комедия", "Отель Белград");
@@ -11,52 +10,70 @@ public class FilmRepositoryTest {
     PosterItems item6 = new PosterItems(6, "Мультфильм", "Тролли. Мировой тур");
     PosterItems item7 = new PosterItems(7, "Комедия", "Номер один");
 
-
     @Test
     public void addNewFilm() {
         FilmRepository repo = new FilmRepository();
-        repo.save(item1);
-        repo.save(item2);
-        repo.save(item3);
-        repo.save(item4);
-        repo.save(item5);
-        repo.save(item6);
-        repo.save(item7);
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
 
         PosterItems[] expected = {item1, item2, item3, item4, item5, item6, item7};
-        PosterItems[] actual = repo.getItems();
+        PosterItems[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
-
     }
 
     @Test
-    public void shouldIdFilm() {
-        FilmRepository repo = new FilmRepository();
-        repo.save(item2);
+    public void shouldAllFilm() {
+        FilmRepository repo = new FilmRepository(7);
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
 
+        PosterItems[] expected = {item7, item6, item5, item4, item3, item2, item1};
+        PosterItems[] actual = repo.findLast();
 
-        int expected = 2;
-        int actual = item2.getId();
-
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    void removeHigherLimitFilm() {
+        FilmRepository repo = new FilmRepository(10);
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
+
+        PosterItems[] expected = {item7, item6, item5, item4, item3, item2, item1};
+        PosterItems[] actual = repo.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
     @Test
-    void removeFilm() {
-        FilmRepository repo = new FilmRepository();
-        repo.save(item1);
-        repo.save(item2);
-        repo.save(item3);
-        repo.save(item4);
-        repo.save(item5);
-        repo.save(item6);
-        repo.save(item7);
-        repo.removeFilm(item2.getId());
+    void removeMinLimitFilm() {
+        FilmRepository repo = new FilmRepository(4);
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
 
-        PosterItems[] expected = {item1, item3, item4, item5, item6, item7};
-        PosterItems[] actual = repo.getItems();
+        PosterItems[] expected = {item7, item6, item5, item4};
+        PosterItems[] actual = repo.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
